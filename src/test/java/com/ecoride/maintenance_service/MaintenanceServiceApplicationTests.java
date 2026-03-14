b927cc001ce7c05d39f0dd0498bbf23ebbdac5c5
nehara-peiris <shewmipeiris123@gmail.com>
2026-03-14 15:02:05 +0530

    Initial commit

---
+++ src/test/java/com/ecoride/maintenance_service/MaintenanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoride.maintenance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
